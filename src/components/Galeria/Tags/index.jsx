efa4ae8c740ea33da04b0e299dff1d80d870276a
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-03-28 17:42:55 -0300

    finalizando projeto

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import styles from './Tags.module.scss'
+import React from "react";
+import styles from "./Tags.module.scss";
+import fotos from "../fotos.json";
 
-export default function Tags() {
+export default function Tags({ tags, filtraFotos, setItens }) {
   return (
     <div className={styles.tags}>
-        <p>Filtre por Tags:</p>
-        <ul className={styles.tags__lista}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planetas</li>
-        </ul>
+      <p>Filtre por Tags:</p>
+      <ul className={styles.tags__lista}>
+        <li onClick={() => setItens(fotos)}>Todas</li>
+        {tags.map((tag) => {
+          return (
+            <li key={tag} onClick={() => filtraFotos(tag)}>
+              {tag}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
